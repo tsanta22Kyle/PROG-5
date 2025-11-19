2ba7842b481215de4fee66d131ff2bb2ded3ac8e
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-11-19 09:35:59 +0300

    chore: add lombok

--- src/main/java/hei/Table.java
+++ src/main/java/hei/Table.java
@@ -1,50 +1,19 @@
 package hei;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter@AllArgsConstructor@EqualsAndHashCode
 public class Table {
     private double longueur;
     private double largeur ;
     private String etat;
     private String couleur;
-    public Table(double longueur, double largeur, String etat, String couleur) {
-        this.longueur = longueur;
-        this.largeur = largeur;
-        this.etat = etat;
-        this.couleur = couleur;
-    }
-
-    public double getLongueur() {
-        return longueur;
-    }
-
-    public void setLongueur(double longueur) {
-        this.longueur = longueur;
-    }
-
-    public double getLargeur() {
-        return largeur;
-    }
-
-    public void setLargeur(double largeur) {
-        this.largeur = largeur;
-    }
-
-    public String getEtat() {
-        return etat;
-    }
-
-    public void setEtat(String etat) {
-        this.etat = etat;
-    }
-
-    public String getCouleur() {
-        return couleur;
-    }
-
-    public void setCouleur(String couleur) {
-        this.couleur = couleur;
-    }
 
     public double getSurface(){
         return this.largeur * this.longueur;
     }
+
 }
